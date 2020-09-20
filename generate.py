b35a5706f3395cdfcc7b06862d283c5874e0ed06
Denis <dmelexin@mail.ru>
2020-09-21 00:02:32 +0300

    ff

--- generate.py
+++ generate.py
@@ -1,21 +1,27 @@
+import argparse
 import pickle
 from model import Model
 import random
 
 
-def read_model():
-    with open('model_data.pickle', 'rb') as f:
+def read_model(path='big_model.pickle'):
+    with open(path, 'rb') as f:
         temp_model = pickle.load(f)
         return temp_model
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--path",
+                    help="Path to model")
+
+
 model = read_model()
 
-start_word = model.data[random.randint(0, len(model) - 1)]
+start_word = [*model.data][random.randint(0, len(model.data) - 1)]
 
 text = start_word + ' '
 for _ in range(40):
-    nw_word = model.data[start_word][random.randint(0, min(len(model[start_word]) - 1, 4))]
+    nw_word = model.data[start_word][random.randint(0, min(len(model.data[start_word]) - 1, 4))]
     text += nw_word + ' '
     start_word = nw_word
 
--- model.py
+++ model.py
@@ -6,7 +6,7 @@ import csv
 class Model:
 
     @staticmethod
-    def read_txt(self, cur_file):
+    def read_txt(cur_file):
         with open(cur_file, 'r') as f:
             text = f.read()
             return text
@@ -23,7 +23,7 @@ class Model:
             return text
 
     @staticmethod
-    def tokenize(self, lst_of_texts):
+    def tokenize(lst_of_texts):
         big_lst = []
         for text in lst_of_texts:
             cur_lst_of_tokens = list(filter(None, text.split(' ')))
@@ -34,24 +34,21 @@ class Model:
         self.language = language
         self.sentence_end = {'!', '.', '\'', '"', ':', ';', ')', '(', '-'}
         self.data = {}
-        self.train()
+        if len(self.data) == 0:
+            self.train()
 
-    def save_text(self, big_lst, words_as_key):
+    def save_text(self, big_lst):
         for cur_lst_of_tokens in big_lst:
             for index, token in enumerate(cur_lst_of_tokens):
-                if index < words_as_key:
+                if index == 0:
                     continue
-                cur_key = ''
-                for i in range(1, words_as_key):
-                    cur_key += cur_lst_of_tokens[index - i]
+                if cur_lst_of_tokens[index - 1] not in self.data:
+                    self.data[cur_lst_of_tokens[index - 1]] = {}
 
-                if cur_key not in self.data:
-                    self.data[cur_key] = {}
-
-                if token in self.data[cur_key]:
-                    self.data[cur_key][token] += 1
+                if token in self.data[cur_lst_of_tokens[index - 1]]:
+                    self.data[cur_lst_of_tokens[index - 1]][token] += 1
                 else:
-                    self.data[cur_key][token] = 1
+                    self.data[cur_lst_of_tokens[index - 1]][token] = 1
 
     def clean_text(self, text):
         lst = []
@@ -72,20 +69,20 @@ class Model:
 
         return lst
 
-    def add_text(self, text, words_as_key):
+    def add_text(self, text):
         text = text.lower()
         lst_of_texts = self.clean_text(text)
         big_lst = self.tokenize(lst_of_texts)
-        self.save_text(big_lst, words_as_key)
+        self.save_text(big_lst)
 
-    def add_data(self, words_as_key):
+    def add_data(self):
         for file in listdir('data/'):
             if file[0] == '.':
                 continue
             if '.csv' in file:
-                self.add_text(self.read_csv(f'data/{file}'), words_as_key)
+                self.add_text(self.read_csv(f'data/{file}'))
             elif '.txt' in file:
-                self.add_text(self.read_txt(f'data/{file}'), words_as_key)
+                self.add_text(self.read_txt(f'data/{file}'))
             print(f'Added {file}')
 
     def sort_data(self, words_for_token=10):
@@ -101,13 +98,16 @@ class Model:
                 temp2.append(elem[1])
             self.data[token] = temp2
 
-    def save_model(self, path='data.pickle'):
+    def save_model(self, path='big_model.pickle'):
         with open(path, 'wb') as f:
-            pickle.dump(self.data, f)
+            pickle.dump(self, f)
 
-    def train(self, words_as_key=2):
+    def train(self):
         print('Starting training!')
-        self.add_data(words_as_key)
+        self.add_data()
         self.sort_data()
-        self.save_data()
+        self.save_model()
         print('Training finished!')
+
+
+temp = Model()
